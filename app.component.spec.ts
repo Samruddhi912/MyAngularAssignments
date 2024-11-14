56a8d8ee138d401ca25ad90b3291b7d43dfa1cb6
Samruddhi912 <146476526+Samruddhi912@users.noreply.github.com>
2024-11-14 15:40:09 +0530

    Add files via upload

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Assignment5_3' title`, () => {
+  it(`should have the 'Assignment5_1' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Assignment5_3');
+    expect(app.title).toEqual('Assignment5_1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Assignment5_3');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Assignment5_1');
   });
 });
--- first-comp.component.ts
+++ first-comp.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { SecondCompComponent } from '../second-comp/second-comp.component';
 
 @Component({
   selector: 'app-first-comp',
   standalone: true,
-  imports: [SecondCompComponent],
+  imports: [],
   templateUrl: './first-comp.component.html',
   styleUrl: './first-comp.component.css'
 })
